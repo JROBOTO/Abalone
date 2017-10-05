4411c1ac56b04e5dc5b970644d4cb6ec5a546722
JROBOTO <J.C.Roberts@student.liverpool.ac.uk>
2017-10-05 11:01:51 +0100

    Started adding multi-device functionality

---
+++ app/src/main/java/com/jcroberts/abalone/Multiplayer.java
@@ -0,0 +1,12 @@
+package com.jcroberts.abalone;
+
+import android.net.wifi.p2p.WifiP2pManager;
+
+/**
+ * Connect to and communicate with another device using peer to peer communication
+ * Author: Joshua Roberts
+ */
+public class Multiplayer {
+    private WifiP2pManager connectionManager;
+
+}
---
+++ app/src/main/java/com/jcroberts/abalone/WifiBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package com.jcroberts.abalone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+
+/**
+ * Receiver to detect important peer to peer connection events
+ * Created by jcrob on 04/10/2017.
+ */
+
+public class WifiBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private Channel mChannel;
+
+    public WifiBroadcastReceiver(WifiP2pManager manager, Channel channel) {
+        super();
+        mManager = manager;
+        mChannel = channel;
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
